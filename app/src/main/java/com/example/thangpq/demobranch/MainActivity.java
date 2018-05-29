1822188082ee11e32902888156f1e47ff0228eaa
DESKTOP-PSU42V2\ThangPQ <thangpham1906@gmail.com>
2018-05-29 08:54:48 +0700

    re

--- app/src/main/java/com/example/thangpq/demobranch/MainActivity.java
+++ app/src/main/java/com/example/thangpq/demobranch/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("ádasdasdasdasdasdasdasdasd");
         System.out.println("ádasdasdasdasdasdasdasdasd");
         System.out.println("ádasdasdasdasdasdasdasdasd");
-        
+
         if("123".equals("123"))
         {
             System.out.println("123");
