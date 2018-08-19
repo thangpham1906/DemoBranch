1210322b11334ac3893506fe833b21be1fe73cc8
thenam1906 <thangpqse04900@fpt.edu.vn>
2018-08-19 11:34:18 +0700

    #124 create class student

---
+++ app/src/main/java/com/example/thangpq/demobranch/Person.java
@@ -0,0 +1,11 @@
+package com.example.thangpq.demobranch;
+
+public class Person {
+    String fistName;
+    String lastName;
+
+    public Person(String fistName, String lastName) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+    }
+}
